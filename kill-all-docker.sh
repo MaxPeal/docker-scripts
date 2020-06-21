9fd4089c2ef1d1f43c9b307bdd126a35dcc22237
MaxPeal <30347730+MaxPeal@users.noreply.github.com>
2020-06-21 18:22:20 +0200

    Update kill-all-docker.sh

--- kill-all-docker.sh
+++ kill-all-docker.sh
@@ -1,9 +1,27 @@
-kill-all-docker() { set -x 
+kill-all-docker() {
+set -x
+echo "#######################################"
 docker system df
+echo "#######################################"
+
 docker ps -a -q | xargs -n 1 -P 8 -I {} docker stop {}
+echo "#######################################"
+
 docker buildx rm
+echo "#######################################"
+
 docker ps -a -q | xargs -n 1 -P 8 -I {} docker rm {}
+echo "#######################################"
+
 docker ps -a -q | xargs -n 1 -P 8 -I {} docker rmi {}
+echo "#######################################"
+
 docker image prune -a -f
+echo "#######################################"
+
 docker volume prune -f
-docker system df }
+echo "#######################################"
+
+docker system df
+echo "#######################################"
+}
