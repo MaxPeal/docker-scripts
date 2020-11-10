2c1b7a8493a01faaedc99e6bfa0f6c022e8b7ef0
MaxPeal <30347730+MaxPeal@users.noreply.github.com>
2020-11-10 11:33:06 +0100

    Update docker-get-all-arch-platform-for-buildx.sh

--- docker-get-all-arch-platform-for-buildx.sh
+++ docker-get-all-arch-platform-for-buildx.sh
@@ -7,7 +7,8 @@ docker buildx ls | cut -d" " -f1 | grep -v "/" | \
 
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
-docker buildx create --use
+#docker buildx create --use
+docker buildx create --name mybuilder --use
 docker buildx inspect --bootstrap
 docker buildx inspect --bootstrap
 
@@ -19,36 +20,36 @@ RUN uname -m
 RUN getconf -a
 EOF
 
-linux/riscv64
+#linux/riscv64
 
 docker manifest inspect alpine
 docker manifest inspect busybox
 docker manifest inspect docker
 docker docker buildx inspect --bootstrap | grep -i Platforms | cut -d":" -f2 | tr -d "[:space:]"
 
- 1055  docker manifest inspect library/docker
- 1056  docker manifest inspect scratch
- 1057  docker pull scratch
- 1058  docker pull library/scratch
- 1059  docker manifest inspect docker.io/library/docker
- 1060  docker manifest inspect docker.io/library/busybox
- 1061  docker manifest inspect docker.io/library/hello-world
- 1062  docker manifest inspect docker.io/library/registry
- 1063  docker manifest inspect docker.io/library/hello-world
- 1064  docker manifest inspect docker.io/library/bash
- 1065  docker manifest inspect docker.io/library/toybox
- 1066  docker manifest inspect docker.io/library/uname
- 1067  docker manifest inspect docker.io/library/ubuntu
- 1068  docker manifest inspect docker.io/library/debian
- 1069  docker manifest inspect docker.io/library/alpine
- 1070  docker manifest inspect docker.io/library/busybox
- 1071  docker buildx inspect
- 1072  buildx ls
- 1073  docker buildx ls
- 1074  docker manifest inspect docker.io/library/buildkit
- 1075  docker manifest inspect docker.io/library/buildKit
- 1076  docker manifest inspect docker.io/library/buildx
- 1077  docker manifest inspect docker.io/library/hello-world
+ # 1055  docker manifest inspect library/docker
+ # 1056  docker manifest inspect scratch
+ # 1057  docker pull scratch
+ # 1058  docker pull library/scratch
+ # 1059  docker manifest inspect docker.io/library/docker
+ # 1060  docker manifest inspect docker.io/library/busybox
+ # 1061  docker manifest inspect docker.io/library/hello-world
+ # 1062  docker manifest inspect docker.io/library/registry
+ # 1063  docker manifest inspect docker.io/library/hello-world
+ # 1064  docker manifest inspect docker.io/library/bash
+ # 1065  docker manifest inspect docker.io/library/toybox
+ # 1066  docker manifest inspect docker.io/library/uname
+ # 1067  docker manifest inspect docker.io/library/ubuntu
+ # 1068  docker manifest inspect docker.io/library/debian
+ # 1069  docker manifest inspect docker.io/library/alpine
+ # 1070  docker manifest inspect docker.io/library/busybox
+ # 1071  docker buildx inspect
+ # 1072  buildx ls
+ # 1073  docker buildx ls
+ # 1074  docker manifest inspect docker.io/library/buildkit
+ # 1075  docker manifest inspect docker.io/library/buildKit
+ # 1076  docker manifest inspect docker.io/library/buildx
+ # 1077  docker manifest inspect docker.io/library/hello-world
 
 
 
